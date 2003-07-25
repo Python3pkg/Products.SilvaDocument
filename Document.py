ba86139e77e65067e6e39690bbc9f7794b1bf667
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2003-07-25 12:29:40 +0000

    remove redundant index_object call - this is already done in the create_version method on VersionedContent.

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 # Zope
 
 from StringIO import StringIO
@@ -223,7 +223,6 @@ def manage_addDocument(self, id, title, REQUEST=None):
     object.manage_addProduct['SilvaDocument'].manage_addDocumentVersion(
         '0', title)
     object.create_version('0', None, None)
-    getattr(object, '0').index_object()
     add_and_edit(self, id, REQUEST)
     return ''
 
