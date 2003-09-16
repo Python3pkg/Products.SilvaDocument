0436340a97767ff0a36264d5e8e5942391b9afc3
Christian Zagrodnick <cz@gocept.com>
2003-09-16 12:02:14 +0000

    added addable priority

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.9 $
+# $Revision: 1.10 $
 # Zope
 
 from StringIO import StringIO
@@ -34,6 +34,7 @@ from Products.Silva.interfaces import IVersionedContent, IContainerPolicy
 from Products.Silva.interfaces import IVersion
 
 icon="www/silvadoc.gif"
+addable_priority = -1
 
 class Document(CatalogedVersionedContent):
     """A Document is the basic unit of information in Silva. A document
