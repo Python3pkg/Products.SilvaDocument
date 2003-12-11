00a0897ac1075f2dc229a27d93c96e6da3fa3523
Christian Zagrodnick <cz@gocept.com>
2003-12-11 14:20:41 +0000

    cleanups and some further optimisations

---
+++ interfaces.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2002 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id: interfaces.py,v 1.1.2.1 2003/12/11 14:20:41 zagy Exp $
+
+from Interface import Interface, Attribute
+
+
+class IParserState(Interface):
+    """State of parsing silva markup"""
+    
+    text = Attribute("complete text to parse and interpret")
+    text_length = Attribute("len(text)")
+    consumed = Attribute("already consumed, i.e. parsed portion of text")
+    tokens = Attribute("""sequence of tokens representing the 
+        consumed text""")
+    parent = Attribute("previous ParserState")
+    kindsum = Attribute("the sum of token ids in self.tokens")
+        
+    def toxml():
+        "returns xml representation of parsed node"
+
+    def valid():
+        "return True if state could result in something interpretable"
+            
+
