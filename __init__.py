ebe925f58eb1982983b676debade476d5c79ca3b
Martijn Faassen <faassen@startifact.com>
2004-07-22 16:41:19 +0000

    Reenable old style imports.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Id: __init__.py,v 1.8 2004/07/22 15:40:16 eric Exp $
+# $Id: __init__.py,v 1.9 2004/07/22 16:41:19 faassen Exp $
 
 from Products.Silva.ExtensionRegistry import extensionRegistry
+from Products.Silva.ImporterRegistry import importer_registry
 import EditorSupportNested
 import ServiceCodeSourceCharset
 import install
@@ -30,14 +31,17 @@ def initialize(context):
                         ServiceCodeSourceCharset.manage_addCodeSourceCharsetService),
         icon = "www/editorservice.gif"
         )
-    
+    # old XML importer
+    importer_registry.register_tag('silva_document', 	 
+                                   Document.xml_import_handler)
     registerDirectory('views', globals())
     registerDirectory('widgets', globals())
     
     registerTypeForMetadata(Document.DocumentVersion.meta_type)
 
     initialize_upgrade()
-    
+
+    # new xml import/export
     xmlexport.initializeXMLExportRegistry()
     xmlimport.initializeXMLImportRegistry()
 
