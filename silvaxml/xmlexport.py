7395ae1763674cb204406d9e11c41573462d0bb4
Eric Casteleijn <ec@trapit.com>
2004-09-06 16:34:16 +0000

    tables are now rendered correctly. That is, they are rendered the same as in the xml widgets renderer. This means multiple paragraphs in a table cell are styled in a very surprising way.

--- silvaxml/xmlexport.py
+++ silvaxml/xmlexport.py
@@ -53,14 +53,139 @@ class DocumentVersionProducer(SilvaBaseProducer):
                     if child.nodeValue:
                         self.handler.characters(child.nodeValue)
                 elif child.nodeType == Node.ELEMENT_NODE:
-                    self.sax_node(child)
+                    if child.nodeName == 'table':
+                        self.sax_table(child)
+                    else:
+                        self.sax_node(child)
         else:
             if node.nodeValue:
                 self.handler.characters(node.nodeValue)
         self.endElementNS(SilvaDocumentNS, node.nodeName)
 
+    def sax_table(self, node):
+        attributes = {}
+        if node.attributes:
+            for key in node.attributes.keys():
+                attributes[key] = node.attributes[key].value
+        self.startElementNS(SilvaDocumentNS, 'table', attributes)
+        columns_info = self.get_columns_info(node)
+        nr_of_columns = len(columns_info)
+        for column in columns_info:
+            col_attributes = {'width': column['html_width'], 'class': 'align-' + column['align']}
+            self.startElementNS(SilvaDocumentNS, 'col' , col_attributes)
+            self.endElementNS(SilvaDocumentNS, 'col')
+        if node.hasChildNodes():
+            row = 0
+            for child in node.childNodes:
+                if child.nodeName == 'row_heading':
+                    self.sax_row_heading(child, nr_of_columns)
+                elif child.nodeName == 'row':
+                    row += 1
+                    self.sax_row(child, row, columns_info)
+        self.endElementNS(SilvaDocumentNS, 'table')
+
+    def sax_row_heading(self, node, nr_of_columns):
+        child_attrs = {'colspan': str(nr_of_columns)}
+        self.startElementNS(SilvaDocumentNS, 'row_heading', child_attrs)
+        if node.hasChildNodes:
+            for child in node.childNodes:
+                if child.nodeType == Node.TEXT_NODE:
+                    if child.nodeValue:
+                        self.handler.characters(child.nodeValue)
+                elif child.nodeType == Node.ELEMENT_NODE:
+                    self.sax_node(child)
+        self.endElementNS(SilvaDocumentNS, 'row_heading')
+        
+    def sax_row(self, node, row, columns_info):
+        child_attrs = {'class': row % 2 and "odd" or "even"}
+        self.startElementNS(SilvaDocumentNS, 'row', child_attrs)
+        if node.hasChildNodes:
+            col = 0
+            for child in node.childNodes:
+                self.sax_field(child, columns_info[col])
+                col += 1
+        self.endElementNS(SilvaDocumentNS, 'row')
+        
+    def sax_field(self, node, col_info):
+        child_attrs = {'class': 'align-' + col_info['align']}
+        self.startElementNS(SilvaDocumentNS, 'field', child_attrs)
+        if node.hasChildNodes:
+            child_count = 0
+            for child in node.childNodes:
+                if child.nodeType == Node.TEXT_NODE:
+                    if child.nodeValue:
+                        self.handler.characters(child.nodeValue)
+                elif child.nodeType == Node.ELEMENT_NODE:
+                    # XXX UGLY EVIL HACK to make this behave the same as 
+                    # the widget renderer, i.e. remove the tags of the
+                    # first child if it is a <p>
+                    if child.nodeName == 'p' and child_count == 0:
+                        if child.hasChildNodes:
+                            for grandchild in child.childNodes:
+                                if grandchild.nodeType == Node.TEXT_NODE:
+                                    if grandchild.nodeValue:
+                                        self.handler.characters(grandchild.nodeValue)
+                                elif grandchild.nodeType == Node.ELEMENT_NODE:
+                                    self.sax_node(grandchild)
+                    else:
+                        self.sax_node(child)
+                child_count += 1
+        self.endElementNS(SilvaDocumentNS, 'field')
+        
+    def get_columns_info(self, node):
+        columns = int(node.getAttribute('columns'))
+        if node.hasAttribute('column_info'):
+            column_info = node.getAttribute('column_info')
+        else:
+            result = []
+            for i in range(columns):
+                result.append({'align': 'left', 'width': 1, 'html_width':'%s%%' % (100/columns) })
+            request.set('columns_info', result)
+            return result
+
+        lookup = { 'L':'left', 'C':'center', 'R': 'right' }
+        
+        result = []
+        for info in column_info.split():
+            info = info.split(':')
+            try:
+                align = info[0]
+            except IndexError:
+                align = 'L'
+            try:
+                width = int(info[1])
+            except IndexError:
+                width = 1
+            except ValueError:
+                width = 1
+            result.append({
+                'align': lookup.get(align, 'L'),
+                'width': width,
+            })
+
+        # too much info, ignore it
+        if len(result) > columns:
+            result = result[:columns]
+        # not enough info, take average and add to missing columns
+        elif len(result) < columns:
+            total = 0
+            for info in result:
+                total += info['width']
+            average = total / len(result)
+            for i in range(columns - len(result)):
+                result.append({'align': 'left', 'width': average })
+
+        # calculate percentages
+        total = 0
+        for info in result:
+            total += info['width']
+        for info in result:
+            percentage = int((float(info['width'])/total) * 100)
+            info['html_width'] = '%s%%' % percentage
+
+        return result
+        
     def sax_source(self, node, id):
-        # XXX Eventually move this over to SilvaExternalSources
         from Products.SilvaExternalSources.ExternalSource import getSourceForId
         source = getSourceForId(self.context, id)
         parameters = {}
