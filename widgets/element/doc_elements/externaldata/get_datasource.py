0842b8ad046d849f53aad35c3064875eb640ba45
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2004-09-30 13:11:46 +0000

    Removing references to now obsolete SQLDataSource.

--- widgets/element/doc_elements/externaldata/get_datasource.py
+++
@@ -1,9 +0,0 @@
-request = context.REQUEST
-node = request.node
-path = node.getAttribute('path').encode('ascii')
-datasource = None
-
-if path:
-    datasource = node.restrictedTraverse(path, None)
-
-return datasource
