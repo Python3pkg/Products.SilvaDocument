3caa27473fe9472ac9f21930829cfb3a6886df65
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2004-12-09 08:19:36 +0000

    Not completely sure about this fix, but it seems to work.

--- views/public/Document/render.py
+++ views/public/Document/render.py
@@ -1,5 +1,2 @@
 content = context.REQUEST.model
-version = content.get_viewable()
-if version is None:
-    return  'Sorry, this Silva Document is not viewable.'
-version.render_view()
+return content.view()
