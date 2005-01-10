267c27f69259e973e8f7936d8211aec4362278ec
Infrae <info@infrae.com>
2005-01-10 17:29:21 +0000

    Tweaked some security assertions.

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.27 $
+# $Revision: 1.28 $
 # Zope
 
 from StringIO import StringIO
@@ -254,7 +254,8 @@ class Document(CatalogedVersionedContent):
             if version_object:
                 self.service_editor.clearCache(version_object.content)
 
-    security.declarePublic('PUT')
+    security.declareProtected(SilvaPermissions.ChangeSilvaContent,
+                                'PUT')
     def PUT(self, REQUEST):
         """PUT support"""
         try:
