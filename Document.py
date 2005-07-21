a66e61e3c0e092b5eb2fc6504c6d0b2b01dd0d63
Walco Van Loon <info@infrae.com>
2005-07-21 07:49:38 +0000

    Fixed API incompatibility with Zope 2.7.7's ZCTextIndex.

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.32 $
+# $Revision: 1.33 $
 # Zope
 
 from StringIO import StringIO
@@ -376,7 +376,8 @@ class DocumentVersion(CatalogedVersion):
         """Return the content of this object without any xml"""
         if self.version_status() == 'unapproved':
             return ''
-        return [self.get_title(), self._flattenxml(self.content_xml())]
+        return ' '.join((self.get_title(),
+            self._flattenxml(self.content_xml())))
 
     security.declareProtected(SilvaPermissions.AccessContentsInformation,
                               'content_xml')
