1250ea909ae6429d236cf2b6c7dae6bcaaefab27
Infrae <info@infrae.com>
2005-09-05 02:41:39 +0000

    tranlsate docstring with the right domain

--- Document.py
+++ Document.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2002-2005 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.34 $
+# $Revision: 1.35 $
 # Zope
 
 from StringIO import StringIO
@@ -42,7 +42,7 @@ from transform.base import Context
 from Products.Silva.interfaces import IVersionedContent, IContainerPolicy
 from Products.Silva.interfaces import IVersion
 from Products.Silva.Versioning import VersioningError
-from Products.Silva.i18n import translate as _
+from Products.SilvaDocument.i18n import translate as _
 
 from Products.SilvaDocument import externalsource
 
