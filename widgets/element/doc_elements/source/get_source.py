1a888448683dcf4ecca74da4de2a7d6842d9c5dc
Infrae <info@infrae.com>
2006-02-02 10:46:21 +0000

    This commit was manufactured by cvs2svn to create tag 'SilvaDocument-1_5'.

--- widgets/element/doc_elements/source/get_source.py
+++
@@ -1,7 +0,0 @@
-request = context.REQUEST
-model = request.model
-node = request.node
-id = node.getAttribute('id').encode('ascii')
-service = context.service_editorsupport
-
-return service.getSourceForId(model, id)
