cc00619c3a3bb85deb9c7fad79051a3600bbd4ac
Infrae <info@infrae.com>
2006-02-10 22:02:14 +0000

    This commit was manufactured by cvs2svn to create tag 'SilvaDocument-1_4_2'.

--- widgets/element/doc_elements/code/mode_edit/save_helper.py
+++
@@ -1,9 +0,0 @@
-from Products.Silva.mangle import String
-
-request = context.REQUEST
-node = request.node
-if request.has_key('path'):
-    path = request['path']
-else:
-    path = '' 
-node.setAttribute('path', String.inputConvert(path))
