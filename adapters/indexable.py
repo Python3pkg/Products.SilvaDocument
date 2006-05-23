b4a51338cc987ab3842ac42410a71f84693d04d6
Eric Casteleijn <ec@trapit.com>
2006-05-23 16:24:24 +0000

    slight refactoring of the indexable adapter:
    there is now an adapter for Document*Versions* as
    well as Documents

--- adapters/indexable.py
+++ adapters/indexable.py
@@ -1,8 +1,14 @@
 from Products.Silva.adapters.indexable import IndexableAdapter
+from Products.Silva.adapters import interfaces
 
 class DocumentIndexableAdapter(IndexableAdapter):
     def getIndexes(self):
         version = self.context.get_viewable()
+        return interfaces.IIndexable(version).getIndexes()
+
+class DocumentVersionIndexableAdapter(IndexableAdapter):
+    def getIndexes(self):
+        version = self.context
         if version is None:
             return []
         indexes = []
@@ -11,4 +17,4 @@ class DocumentIndexableAdapter(IndexableAdapter):
         for node in nodes:
             indexName = node.getAttribute('name')
             indexes.append(indexName)
-        return indexes
+        return indexes        
