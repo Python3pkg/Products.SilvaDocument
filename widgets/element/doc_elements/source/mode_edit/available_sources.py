892b6fd2b4fd36920ac6cd4c6677055e66437149
Eric Casteleijn <ec@trapit.com>
2006-10-20 16:14:26 +0000

    forms editor sorts sources correctly again

--- widgets/element/doc_elements/source/mode_edit/available_sources.py
+++ widgets/element/doc_elements/source/mode_edit/available_sources.py
@@ -1,12 +1,2 @@
-request = context.REQUEST
-model = request.model
-service = context.service_editorsupport
+return context.service_editorsupport.availableSources(context)
 
-ctxt = model.get_container()
-
-sources = []
-for id, source in service.availableSources(ctxt):
-    sources.append(source)
-
-sources.sort(lambda s1, s2: cmp(s1.title, s2.title))
-return sources
