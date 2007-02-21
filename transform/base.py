2bad26aaa405c203e49e2907aea11367b579c092
Eric Casteleijn <ec@trapit.com>
2007-02-21 11:55:29 +0000

    version.txt and fixed a raised string to a real error

--- transform/base.py
+++ transform/base.py
@@ -69,7 +69,7 @@ class Node:
             return issubclass(self.__class__, tag)
 
     def __eq__(self, other):
-        raise "not implemented, override in inheriting class"
+        raise NotImplementedError, "not implemented, override in inheriting class"
 
     def name(self):
         """ return name of tag """
