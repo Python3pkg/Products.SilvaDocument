55800624610fa7b252a7b0a0f896ce4ba28f44bf
Eric Casteleijn <ec@trapit.com>
2008-02-05 14:15:13 +0000

    copyright fixed

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2002-2007 Infrae. All rights reserved.
+# Copyright (c) 2002-2008 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id: test_upgrade.py,v 1.5 2006/01/24 16:15:05 faassen Exp $
 
