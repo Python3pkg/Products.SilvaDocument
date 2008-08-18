baf18adca8c6e173eeb433671a22283f36db2010
Todd Matsumoto <info@infrae.com>
2008-08-18 07:18:59 +0000

    - add pure firefox clip board fragment

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -45,26 +45,17 @@ class TransformerTest(SilvaTestCase):
                           ms_doc1,
                           context=context)
 
-        ms_doc2 = """<html>
-        <head><title></title>
-        <link href="http://localhost:8087/silva_tranformer_test/frontend.css" type="text/css" rel="stylesheet" />
-        <link href="http://localhost:8087/silva_tranformer_test/globals/kupu.css" type="text/css" rel="stylesheet" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="docref" content="x%DA%D3%60f%60%60%28%01b%86bQ%10%91%99S%96%18_R%94%98%97%96_%94%9BZ%14_%92Z%5CR%CC%01RS%9C%98%92%93%9D%96%05%00%1A%D7%0D%D3" />
-        </head>
-        <body>
-        <h2></h2>
+        ms_doc2 = """<p class="MsoNormal" style="margin-left: 35.4pt; text-indent: -35.4pt;"><span style="" lang="EN-US">Dit <span style="color: red;">is</span>
+<b style="">een</b> </span><i style=""><span style="font-size: 24pt;" lang="EN-US">test</span></i><span style="" lang="EN-US"><o:p></o:p></span></p>
 
 
-        <p>
-        <span lang="EN-US">Dit <span>is</span>
-        <strong>een</strong>
-        </span>
-        </p>
-        </body>
-        </html>"""
-        src = str(transformer.to_source(ms_doc2, context=context))
-        print src
+<h2 silva_id="new_test" silva_origin="silva_document"></h2>
+<p class="normal">          &lt;p class="MsoNormal" style="margin-left: 35.4pt; text-indent: -35.4pt;"&gt;&lt;span style="" lang="EN-US"&gt;Dit &lt;span style="color: red;"&gt;is&lt;/span&gt;<br>&lt;b style=""&gt;een&lt;/b&gt; &lt;/span&gt;&lt;i style=""&gt;&lt;span style="font-size: 24pt;" lang="EN-US"&gt;test&lt;/span&gt;&lt;/i&gt;&lt;span style="" lang="EN-US"&gt;&lt;o:p&gt;&lt;/o:p&gt;&lt;/span&gt;&lt;/p&gt;</p>
+<p class="normal"><br> Dit is <b>een</b> <i>test</i></p>"""
+        self.assertRaises(ExpatError,
+                          transformer.to_source,
+                          ms_doc2,
+                          context=context)
 
 def test_suite():
     suite = unittest.TestSuite()
