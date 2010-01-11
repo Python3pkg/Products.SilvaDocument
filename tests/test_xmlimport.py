ea2570a3a42bacc4c80e529e24dda0fe5127d633
Sylvain Viollon <thefunny@gmail.com>
2010-01-11 14:48:33 +0000

    Update tests since we removed silva-extra:location

--- tests/test_xmlimport.py
+++ tests/test_xmlimport.py
@@ -41,12 +41,6 @@ class ImportTestCase(SilvaTestCase.SilvaTestCase):
             document_version.get_title(),
             'This is (surprise!) a document')
 
-        metadata_service = self.root.service_metadata
-        binding = metadata_service.getMetadata(document_version)
-        self.assertEquals(
-           binding._getData('silva-extra').data['location'],
-            'http://nohost/root/testfolder/test_document')
-
         doc = document_version.content.documentElement.__str__()
         self.assertEquals(doc,
         u'<doc>\n            <p>\n            <em>\u627f\u8afe\u5e83\u544a\uff0a\u65e2\u306b\u3001\uff12\u5104\u3001\uff13\u5104\u3001\uff15\u5104\uff19\u5343\u4e07\u5186\u53ce\u5165\u8005\u304c\u7d9a\u51fa<strong>boo</strong>\n              baz</em>\n            </p>\n          </doc>')
