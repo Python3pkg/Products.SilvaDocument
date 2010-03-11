b116fa0f85f8c2c459a0c207cbf1272eaf1ab24d
Sylvain Viollon <thefunny@gmail.com>
2010-03-11 17:14:57 +0000

    Update interface.

--- interfaces.py
+++ interfaces.py
@@ -5,37 +5,53 @@
 from zope.interface import Interface, Attribute
 from silva.core.interfaces import IVersionedContent, IVersion
 
+
 class IMixedContentSupport(Interface):
     """ Support of editing and rendering mixed content XML (i.e.
     XML elements containing both text- and child nodes).
     """
-    
+
     _node = Attribute("XXX something on the node attritbute here")
-    
+
     def parse(input_string):
         """ Parse the user input to a DOM fragment for inclusion
         in the Document being edited.
-        
+
         Complementary to render_editable().
         """
-        pass
-    
+
     def renderHTML(view_type='public'):
         """ Render the node to HTML.
-        
+
         Specify the view_type for this render: 'public' or 'edit'.
         """
-        pass
-    
+
     def renderEditable():
         """ Render the node to user editable text.
-        
+
         Complementary to parse().
         """
-        pass
 
 class IDocument(IVersionedContent):
-    pass
+    """A document let you store and edit rich text using different editors.
+    """
+
 
 class IDocumentVersion(IVersion):
-    pass
+    """This is a version of a document.
+    """
+
+    def get_document_xml():
+        """Return the document XML of the document.
+        """
+
+    def get_document_xml_as(format='kupu', request=None):
+        """Return the XML of the document converted in a different
+        format. By default it will be HTML that can be edited by Kupu.
+        """
+
+    def set_document_xml_from(context, format='kupu', request=None):
+        """This change the document XML to content. Content is
+        converted to the document XML from the input format, which is
+        Kupu by default.
+        """
