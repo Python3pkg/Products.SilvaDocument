1f83ab7d81777f70452520f5e1aec148aa87df6b
Sylvain Viollon <thefunny@gmail.com>
2010-06-01 16:08:19 +0000

    Fix tests, add test on XMLImport.

--- tests/test_xmlimport.py
+++ tests/test_xmlimport.py
@@ -5,45 +5,64 @@
 
 import unittest
 
+from zope.component import getUtility
+
+from Products.SilvaDocument.interfaces import IDocument
+from Products.SilvaMetadata.interfaces import IMetadataService
+
 from Products.Silva.testing import FunctionalLayer
 from Products.Silva.tests.helpers import open_test_file
+from Products.Silva.tests.test_xmlimport import SilvaXMLTestCase
 from Products.Silva.silvaxml import xmlimport
 
 
-class ImportTestCase(unittest.TestCase):
+class XMLImportTestCase(SilvaXMLTestCase):
     """Test the import of a document.
     """
     layer = FunctionalLayer
 
     def setUp(self):
         self.root = self.layer.get_application()
+        self.layer.login('editor')
         factory = self.root.manage_addProduct['Silva']
         factory.manage_addFolder('folder', 'Folder')
+        self.metadata = getUtility(IMetadataService)
 
-    def test_document_import(self):
-        importfolder = self.root.folder
-        importer = xmlimport.theXMLImporter
-        source_file = open_test_file('test_document.xml', globals(), 'r')
-        test_settings = xmlimport.ImportSettings()
-        test_info = xmlimport.ImportInfo()
-        importer.importFromFile(
-            source_file,
-            result=importfolder,
-            settings=test_settings,
-            info=test_info)
-        source_file.close()
+    def test_document(self):
+        """Import a simple document.
+        """
+        self.import_file('test_import_document.silvaxml', globals())
+        self.assertListEqual(self.root.folder.objectIds(), ['folder'])
+        self.assertListEqual(self.root.folder.folder.objectIds(), ['document'])
 
-        document_version = importfolder.testfolder.test_document.get_editable()
-        self.assertEquals(
-            document_version.get_title(),
-            'This is (surprise!) a document')
+        document = self.root.folder.folder.document
+        self.failUnless(IDocument.providedBy(document))
 
-        doc = document_version.content.documentElement.__str__()
-        self.assertEquals(doc,
-        u'<doc>\n            <p>\n            <em>\u627f\u8afe\u5e83\u544a\uff0a\u65e2\u306b\u3001\uff12\u5104\u3001\uff13\u5104\u3001\uff15\u5104\uff19\u5343\u4e07\u5186\u53ce\u5165\u8005\u304c\u7d9a\u51fa<strong>boo</strong>\n              baz</em>\n            </p>\n          </doc>')
+        version = document.get_editable()
+        self.failIf(version is None)
+        self.assertEqual(document.get_viewable(), None)
+        self.assertEqual(version.get_title(), u'Previewing a document')
+
+        binding = self.metadata.getMetadata(version)
+        self.assertEqual(binding.get('silva-extra', 'creator'), u'wimbou')
+        self.assertEqual(binding.get('silva-extra', 'lastauthor'), u'wimbou')
+        self.assertEqual(
+            binding.get('silva-content', 'maintitle'), u'Previewing a document')
+        self.assertEqual(
+            binding.get('silva-extra', 'content_description'),
+            u'How to click on preview.')
+        self.assertEqual(
+            binding.get('silva-extra', 'comment'),
+            u'Caution: A special skill-set is required for this operation.')
+
+        with open_test_file(
+            'test_imported_document.docxml', globals()) as expected_document:
+            self.assertXMLEqual(
+                str(version.content.documentElement),
+                expected_document.read())
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ImportTestCase))
+    suite.addTest(unittest.makeSuite(XMLImportTestCase))
     return suite
