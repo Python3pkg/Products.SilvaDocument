96fc40f35250280e44a0ed95e502e8d11ec07b88
Antonin Amand <antonin.amand@gmail.com>
2010-12-06 17:55:38 +0100

    add save editor form

--- Products/SilvaDocument/smi/edit.py
+++ Products/SilvaDocument/smi/edit.py
@@ -5,6 +5,8 @@ from silva.core.smi import interfaces
 from silva.core.layout.jquery.interfaces import IJQueryResources
 from Products.SilvaDocument.interfaces import IDocument
 from silva.core import conf as silvaconf
+import zeam.form.silva as silvaforms
+from zExceptions import BadRequest
 
 grok.layer(interfaces.ISMILayer)
 
@@ -24,7 +26,7 @@ class SMITabEdit(silvasmi.SMIPage):
     grok.implements(interfaces.IEditTab,
                     interfaces.ISMITabIndex,
                     interfaces.ISMINavigationOff)
-    grok.name('tab_edit')
+    grok.name('tab_edit_cke')
 
     tab = 'edit'
 
@@ -33,4 +35,30 @@ class SMITabEdit(silvasmi.SMIPage):
         self.document = self.context.get_editable().get_document_xml_as(
             request=self.request)
 
+
+class EditorSaveDocument(silvasmi.SilvaForm):
+    """ A form to save the document
+    """
+    grok.context(IDocument)
+    grok.name('editor_save')
+
+    fields = silvaforms.Fields(
+        silvaforms.Field('content', required=True))
+    ignoreContent = True
+    ignoreRequest = False
+    tab = 'edit'
+
+    @silvaforms.action('save', method="post")
+    def save(self):
+        data, errors = self.extractData()
+        if errors:
+            raise BadRequest(errors)
+        version = self.context.get_editable()
+        if version is None:
+            raise BadRequest('invalid version')
+        version.set_document_xml_from(data['content'])
+        return silvaforms.SUCCESS
+
+    def render(self):
+        return u''
 
