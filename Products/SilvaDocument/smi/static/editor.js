55ad32e519d780bf038bfd7bfaa4b6cf9f471733
Antonin Amand <antonin.amand@gmail.com>
2010-12-16 17:21:46 +0100

    add ajax save

--- Products/SilvaDocument/smi/static/editor.js
+++ Products/SilvaDocument/smi/static/editor.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
     var element = $('#editor textarea');
     $.each($('div.externalsource', $(element)), function(index, item) {
-      console.log(item);
       $(item).attr('contenteditable', 'false');
     });
-    element.ckeditor(function(){}, {toolbar: 'Full'});
+    element.ckeditor(function(){}, {
+      entities: false,
+      toolbar: 'silva',
+      toolbar_silva: [
+        ['SilvaAjaxSave', '-', 'Source'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+        '/',
+        ['Styles','Format'],
+        ['Bold','Italic','Strike'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['Link','Unlink','Anchor'],
+        ['Maximize','-','About']
+      ],
+      extraPlugins: 'silvaajaxsave'
+    });
 });
