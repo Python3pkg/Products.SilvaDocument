bd1222338f1f65bad63eaec721cf664fbf00b7ff
Sylvain Viollon <thefunny@gmail.com>
2012-06-15 17:32:51 +0200

    Fix test.

--- Products/SilvaDocument/tests/test_xsltrendering.py
+++ Products/SilvaDocument/tests/test_xsltrendering.py
@@ -75,7 +75,7 @@ class CodeSourceXSLTRenderingTestCase(XSLTRenderingTestCase):
         """
         self.layer.login('manager')
         if parameters is None:
-            parameters = '##parameters=model,version'
+            parameters = '##parameters=model,version,request'
         script = self.root.codesource.script
         script.write('\n'.join((parameters, '', script_data, '')))
         self.layer.login('author')
